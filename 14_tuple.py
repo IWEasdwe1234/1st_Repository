f903444bd1920ff1e7669c6548cd5d85c7ff574e
IWEasdwe1234 <asdwe1234@gmail.com>
2026-07-30 17:39:16 +0900

    튜플의 인덱싱, 슬라이싱, 언패킹

--- 14_tuple.py
+++ 14_tuple.py
@@ -56,3 +56,72 @@ print("type(sensor): ", type(sensor))  # <class 'tuple'>
 
 # (1, 2, 3,) -> 가장 마지막에 쉼표를 붙여서 튜플임을 명시
 # (1, 2, 3) -> 튜플 맞음
+
+
+# 튜플의 인덱스
+print("\n튜플의 인덱스")
+
+sensor = ["모터온도", 78]
+print(sensor[0])
+
+# 튜플의 슬라이싱
+print("\n튜플의 슬라이싱")
+
+s = (
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+)
+print(s[1:4])
+# 슬라이싱한 결과는 소괄호에 감싸져 있음
+# 튜플은 슬라이싱해도 결과가 튜플
+print(type(s[1:4]))  # <class 'tuple'>
+
+# 튜플 언패킹
+print("\n튜플의 언패킹")
+# 튜플에 담긴 값을 변수로 한 번에 분리
+
+# 복습) 복수의 변수 한 번에 선언
+a, b, c = "a", "b", "c"
+print(a)  # 문자열 a
+print(b)  # 문자열 b
+print(c)  # 문자열 c
+
+unpacking = (
+    1,  # 변수 one
+    2,  # 변수 two
+    3,  # 변수 three
+)
+
+# unpacking = one, two, three
+# one two three라는 알 수 없는 변수를
+# unpaking 변수에 할당하겠다는 의미
+# 동작X
+
+one, two, three = unpacking
+# unpacking이라는 변수에 담긴 튜플 내부의 값들을
+# 할당 연산자 왼쪽 one, two, three 변수에
+# 풀어서 담는다는 뜻
+print("one:", one)
+print("two:", two)
+print("three:", three)
+
+
+# print("one:", one)
+# print("two:", two)
+# print("three:", three)
+# print("four:", four)
+# one, two, three, four = unpacking
+# 튜플의 언패킹은 변수의 개수와
+# 튜플에 담긴 값의 개수가 동일해야 함
+print("\n")
+
+# 리스트 언패킹이 가능할까?
+one, two, three, four = [11, 22, 33, 44]
+print("one:", one)
+print("two:", two)
+print("three:", three)
+print("four:", four)
+# 가능!
