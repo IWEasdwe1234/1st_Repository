f237542bb13429cc4c41edd934bac325a31a3347
IWEasdwe1234 <asdwe1234@gmail.com>
2026-07-30 17:21:02 +0900

    튜플의 기준과 예시

--- 14_tuple.py
+++ 14_tuple.py
@@ -6,6 +6,53 @@
 # == 튜플 만들기 ==
 print("\n== 튜플 만들기 ==")
 
-sensor = ("모터온도", 78)
+# 예시 1)
+print("\n괄호 있고, 끝에 쉼표 없음")
+sensor = ("모터온도", 78)  # 괄호 있고, 끝에 쉼표 없음
 print("sensor: ", sensor)
-print("type(sensor): "type(sensor))
+print("type(sensor): ", type(sensor))  # <class 'tuple'>
+
+# 예시 2)
+print("\n괄호 없고, 끝에 쉼표 없음")
+sensor = "모터온도", 78  # 괄호 없고, 끝에 쉼표 없음
+print("sensor: ", sensor)
+print("type(sensor): ", type(sensor))  # <class 'tuple'>
+
+# 예시 3)
+print("\n괄호 있고, 끝에 쉼표 있음")
+sensor = (
+    "모터온도",
+    78,
+)  # 괄호 있고, 끝에 쉼표 있음
+print("sensor: ", sensor)
+print("type(sensor): ", type(sensor))  # <class 'tuple'>
+
+# 예시 4)
+print("\n괄호 없고, 끝에 쉼표 없음")
+sensor = 78  # 괄호 없고, 끝에 쉼표 없음
+print("sensor: ", sensor)
+print("type(sensor): ", type(sensor))  # <class 'int'>
+
+# 예시 5)
+print("\n괄호 있고, 끝에 쉼표 있음")
+sensor = (78,)  # 괄호 있고, 끝에 쉼표 있음
+print("sensor: ", sensor)
+print("type(sensor): ", type(sensor))  # <class 'tuple'>
+
+# 예시 6)
+print("\n괄호 있고, 끝에 쉼표 없고, 값도 안담김")
+sensor = ()  # 괄호 있고, 끝에 쉼표 없고, 값도 안담김
+print("sensor: ", sensor)
+print("type(sensor): ", type(sensor))  # <class 'tuple'>
+
+# 요소 갯수
+# 요소 2개 이상: 쉼표가 있다면 튜플
+# 요소 1개: 쉼표 여부
+# 요소0개(빈 튜플): () 빈 괄호
+
+# 튜플에서 많이 헷갈려하는 부분
+# (1): int
+# (1,): tuple
+
+# (1, 2, 3,) -> 가장 마지막에 쉼표를 붙여서 튜플임을 명시
+# (1, 2, 3) -> 튜플 맞음
