cf301b274a1c00f4c928311afd87567a9a417eb6
IWEasdwe1234 <asdwe1234@gmail.com>
2026-08-06 11:05:24 +0900

    구분선 코드 단순화

--- test.py/file_test_01.py
+++ test.py/file_test_01.py
@@ -1,5 +1,5 @@
 # ========================================
-print("\n" + "=" * 40 + "\n")
+print(f"\n{"=" * 40}\n")
 
 # 실습 open으로 파일을 열어 read, readlines로 내용을 읽기
 print("실습 open으로 파일을 열어 read, readlines로 내용을 읽기")
@@ -13,7 +13,7 @@ print("실습 open으로 파일을 열어 read, readlines로 내용을 읽기")
 #   ④ 두 방식의 결과 차이를 비교하고 파일을 close
 
 # ========================================
-print("\n" + "=" * 40 + "\n")
+print(f"\n{"=" * 40}\n")
 
 
 # ① open으로 파일을 읽기 모드 r, utf-8로 열기
@@ -22,7 +22,7 @@ print(" ① open으로 파일을 읽기 모드 r, utf-8로 열기\n")
 f = open("data/sample.txt", "r", encoding="utf-8")
 
 # ----------------------------------------
-print("\n" + "-" * 40 + "\n")
+print(f"\n{"-" * 40}\n")
 
 # ② read로 전체를 한 문자열로 읽어 출력
 print(" ② read로 전체를 한 문자열로 읽어 출력\n")
@@ -31,7 +31,7 @@ f = open("data/sample.txt", "r", encoding="utf-8")
 print(f.read())
 
 # ----------------------------------------
-print("\n" + "-" * 40 + "\n")
+print(f"\n{"-" * 40}\n")
 
 # ③ readlines로 줄 리스트로 읽어 출력
 print(" ③ readlines로 줄 리스트로 읽어 출력\n")
@@ -40,7 +40,7 @@ f = open("data/sample.txt", "r", encoding="utf-8")
 print(f.readlines())
 
 # ----------------------------------------
-print("\n" + "-" * 40 + "\n")
+print(f"\n{"-" * 40}\n")
 # ④ 두 방식의 결과 차이를 비교하고 파일을 close
 print(" ④ 두 방식의 결과 차이를 비교하고 파일을 close\n")
 
