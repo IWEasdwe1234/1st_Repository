2a8caf0eee36845db32b42b3e018c7f3d914b28d
IWEasdwe1234 <asdwe1234@gmail.com>
2026-08-10 14:19:30 +0900

    ndim, shap, size, dtype 학습

--- 23_numpy_02_2d.py
+++ 23_numpy_02_2d.py
@@ -28,3 +28,22 @@ print(dim_2_array[1][1])  # 7
 # 2차원 배열 내용을 가리키는게 일반적
 print(dim_2_array[0, 0])  # 1
 print(dim_2_array[1, 1])  # 7
+
+# ====================
+print(f"\n{"="*20}\n")
+
+# 이 배열은 몇 차원인지 알아보기
+print(dim_2_array.ndim)  # 2
+
+# 이 배열의 형태 알아보기 (몇 행, 몇 컬럼)
+print(dim_2_array.shape)  # (4, 5)
+
+# 배열의 크기 알아보기 (보통 행 * 컬럼)
+print(dim_2_array.size)  # 20 = 4 * 5
+
+# 배열 안의 내용물이 어떤 타입인지 알아보기
+print(dim_2_array.dtype)  # int64
+
+
+# ====================
+print(f"\n{"="*20}\n")
