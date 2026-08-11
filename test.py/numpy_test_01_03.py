683dfb9149105e938651aa1c918ed4ce73d49589
IWEasdwe1234 <asdwe1234@gmail.com>
2026-08-11 14:10:24 +0900

    배열 연산과 필터링 학습

--- test.py/numpy_test_01_03.py
+++ test.py/numpy_test_01_03.py
@@ -7,16 +7,16 @@ import numpy as np
 
 # 단계
 # 회전수 측정 배열 준비
-rpm3 = np.array([1551, 1408, 1498, 1433, 1425, 2861])
+rpm = np.array([1551, 1408, 1498, 1433, 1425, 2861])
 
 
 # 최솟값과 최댓값을 min, max로 확인
-print(rpm3.min())  # 1408
-print(rpm3.max())  # 2861
+print(rpm.min())  # 1408
+print(rpm.max())  # 2861
 
 # 정규화 공식을 브로드캐스팅으로 적용해 변환
 # 정규화 공식
 # 정규화된x = (비교대상 - 최솟값) / (최댓값 - 최솟값)
-x = (rpm3 - rpm3.min()) / (rpm3.max() - rpm3.min())
+x = (rpm - rpm.min()) / (rpm.max() - rpm.min())
 print(x)
 print(np.round(x, 2))
