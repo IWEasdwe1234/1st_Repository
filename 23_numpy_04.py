9dd52c2d154097d7efbdb847e5dbc66fd22d5e16
IWEasdwe1234 <asdwe1234@gmail.com>
2026-08-11 11:37:53 +0900

    배열 관련 내용 학습2

--- 23_numpy_04.py
+++ 23_numpy_04.py
@@ -114,3 +114,46 @@ base = np.array([70, 2.0])
 
 # table의 각 행에서 기준값(base)을 빼기
 print(table - base)
+
+# ====================
+print(f"\n{"="*20}\n")
+
+print(f"비교 연산과 불리언 배열\n")
+
+v = np.array([70, 95, 71, 88, 73])
+print(v > 85)  # [False  True False  True False]
+
+# ----------
+print(f"\n{"-"*10}\n")
+
+# Boolean indexing
+# 불리언 배열로 조건에 맞는 값만 골라내기
+print(v[v > 85])  # [95 88]
+
+# ----------
+print(f"\n{"-"*10}\n")
+
+# np.where 조건 처리
+# 조건에 따라 값을 둘 중 하나로 바꾸기
+# - 조건/참/거짓 ... 세 가지 인자
+# 조건이 참이면 1(위험)
+# 조건이 0(정상)
+
+print(np.where(v > 85, 1, 0))
+# [0 1 0 1 0]
+
+# ----------
+print(f"\n{"-"*10}\n")
+
+# 다중 조건 결합
+print(v)  # [70 95 71 88 73]
+v_step1 = v[v > 70]
+print(v_step1)  # [95 71 88 73]
+v_step2 = v_step1[v_step1 < 90]
+print(v_step2)  # [71 88 73]
+
+v_mixed = v[(v > 70) & (v < 90)]
+print(v_mixed)  # [71 88 73]
+
+# 참고, 조건 대신 True를 직접 준다면?
+print(v[True])  # [[70 95 71 88 73]]
